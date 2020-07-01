7739f44a757dbbc2b34edb0f62181230aeb523f1
prathamesh-p11 <prathameshp2472@gmail.com>
2020-06-30 23:27:31 -0700

    LineTracing for grabber

--- Source/Escape/Grabber.cpp
+++ Source/Escape/Grabber.cpp
@@ -1,7 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "DrawDebugHelpers.h"	//to draw the reach vector
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+
+//macro to mark out parameters, OUT does nothing, its only function is to mark out parameters for readability
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -29,6 +34,30 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
+	// Get player's viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);	
 
+	//Log
+	// UE_LOG(LogTemp, Warning, TEXT("Player view point location = %s"), *PlayerViewPointLocation.ToString());
+	// UE_LOG(LogTemp, Warning, TEXT("Player view point rotation = %s"), *PlayerViewPointRotation.ToString());
+	
+	//Draw a line from player to show reach
+	FVector LineTraceEndRotation = PlayerViewPointRotation.Vector(); 
+	FVector LineTraceEnd = PlayerViewPointLocation + (LineTraceEndRotation * Reach);
+	DrawDebugLine(
+		GetWorld(),		//world
+		PlayerViewPointLocation,		//starting point
+		LineTraceEnd,					//end point
+		FColor(0, 255, 0),				//color of line
+		false,							//does line persists? false - we want to update the line every tick
+		0.f,							//lifetime - no persistence - no life - 0
+		0,								//Depthpriority
+		5								//line thickness
+	);
+
+
+	//Reach out from player(RayCasting) to a certain distance
+	//What objects rays are hitting and are they grabable 
+}
